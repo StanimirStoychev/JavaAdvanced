e78988afe9ae5656d1181616b7ae8e8eb797bbe1
Stanimir <stmbksa@gmail.com>
2022-05-22 10:59:43 +0200

    Bug fixed

--- src/StacksAndQueues/MatchingBrackets.java
+++ src/StacksAndQueues/MatchingBrackets.java
@@ -1,3 +1,5 @@
+package StacksAndQueues;
+
 import java.util.ArrayDeque;
 import java.util.Deque;
 import java.util.Scanner;
@@ -8,15 +10,15 @@ public class MatchingBrackets {
 
         String expression = scanner.nextLine();
 
-        Deque<Integer> stack = new ArrayDeque<>();
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
 
         for (int i = 0; i < expression.length(); i++) {
             char ch = expression.charAt(i);
-            if (ch == ')') {
+            if (ch == '(') {
             stack.push(i);
             } else if (ch == ')') {
-                int startIndex = stack.pop();
-                String contents = expression.substring(startIndex, i + 1);
+                int lastOpenIndex = stack.pop();
+                String contents = expression.substring(lastOpenIndex, i + 1);
                 System.out.println(contents);
             }
         }
