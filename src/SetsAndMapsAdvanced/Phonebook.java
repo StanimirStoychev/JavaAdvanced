f724752ce24cc49389ff0a651618b481c8dca4ff
Stanimir <stmbksa@gmail.com>
2022-05-28 23:47:04 +0300

    new problems solved.

---
+++ src/SetsAndMapsAdvanced/Phonebook.java
@@ -0,0 +1,26 @@
+package SetsAndMapsAdvanced;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+public class Phonebook {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Map<String, String> phonebook = new HashMap<>();
+        String command;
+
+        while (!"search".equals(command = scanner.nextLine())) {
+            String[] info = command.split("-");
+            phonebook.put(info[0], info[1]);
+        }
+
+        while (!"stop".equals(command = scanner.nextLine())) {
+            if (phonebook.containsKey(command)) {
+                System.out.printf("%s -> %s%n",command, phonebook.get(command));
+            } else {
+                System.out.printf("Contact %s does not exist.%n", command);
+            }
+        }
+    }
+}
