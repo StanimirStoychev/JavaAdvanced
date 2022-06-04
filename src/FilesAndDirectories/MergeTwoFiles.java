68a49af1996c5d6a57af22b06abd1137e270fee7
Stanimir <stmbksa@gmail.com>
2022-06-04 14:00:16 +0300

    New theme, problems solved.

---
+++ src/FilesAndDirectories/MergeTwoFiles.java
@@ -0,0 +1,24 @@
+package FilesAndDirectories;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
+
+public class MergeTwoFiles {
+    public static void main(String[] args) {
+        Path input1 = Paths.get("src/resources/inputOne.txt");
+        Path input2 = Paths.get("src/resources/inputTwo.txt");
+        Path output = Paths.get("src/resources/output.txt");
+        try {
+            List<String> firstFile = Files.readAllLines(input1);
+            List<String> secondFile = Files.readAllLines(input2);
+            Files.write(output, firstFile, StandardOpenOption.APPEND);
+            Files.write(output, secondFile, StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
