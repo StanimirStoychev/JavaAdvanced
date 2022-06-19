891ca6bb59a306531dd9e9b00482471ff930e741
Stanimir <stmbksa@gmail.com>
2022-06-19 16:20:25 -0400

    new theme

---
+++ src/IteratorsAndComparators/ListyIterator/ListyIterator.java
@@ -0,0 +1,33 @@
+package IteratorsAndComparators.ListyIterator;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ListyIterator {
+    private List<String> elements;
+    private int currentIndex;
+
+    public ListyIterator(String... elements) {
+        this.elements = List.of(elements);
+        currentIndex = 0;
+    }
+
+    public boolean hasNext() {
+        return currentIndex < elements.size() - 1;
+    }
+
+    public boolean move() {
+        if(hasNext()) {
+            this.currentIndex++;
+            return true;
+        }
+        return false;
+    }
+
+    public void print() {
+        if (this.elements.isEmpty()) {
+            throw new IllegalStateException("Invalid operation");
+        }
+        System.out.println(this.elements.get(currentIndex));
+    }
+}
