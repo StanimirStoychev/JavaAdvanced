3a1de3a49429e66038cd9f7c5183bfd360b30491
Stanimir <stmbksa@gmail.com>
2022-06-23 19:20:16 +0300

    New problem solved

---
+++ src/ExamPreparation/Bomb.java
@@ -0,0 +1,98 @@
+package ExamPreparation;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Bomb {
+
+    private static int row;
+    private static int col;
+    private static int bombsHit;
+    private static int allBombs;
+    private static boolean end = false;
+    private static boolean haveBombs = true;
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int size = Integer.parseInt(scanner.nextLine());
+        String[] commands = scanner.nextLine().split(",");
+        char[][] matrix = new char[size][size];
+
+        for (int i = 0; i < size; i++) {
+            List<Character> characters = Arrays.stream(scanner.nextLine().split("\\s+"))
+                    .map(e -> e.charAt(0))
+                    .collect(Collectors.toList());
+            for (int j = 0; j < characters.size(); j++) {
+                char currentChar = characters.get(j);
+                if (currentChar == 's') {
+                    row = i;
+                    col = j;
+                }
+                if (currentChar == 'B') {
+                    allBombs++;
+                }
+                matrix[i][j] = currentChar;
+            }
+        }
+
+        for (int i = 0; i < commands.length; i++) {
+            String currentCommand = commands[i];
+        
+            switch (currentCommand) {
+                case "up":
+                    movePosition(matrix, -1, 0);
+                    break;
+                case "down":
+                    movePosition(matrix, 1, 0);
+                    break;
+                case "left":
+                    movePosition(matrix, 0, -1);
+                    break;
+                case "right":
+                    movePosition(matrix, 0, 1);
+                    break;
+            }
+            if (matrix[row][col] == 'B') {
+                bombsHit++;
+                System.out.println("You found a bomb!");
+            }
+            if (end) {
+                System.out.printf("END! %d bombs on the field", allBombs - bombsHit);
+                break;
+            } else if (allBombs - bombsHit == 0) {
+                System.out.println("Congratulations! You found all bombs!");
+                haveBombs = false;
+                break;
+            }
+        }
+
+        if (end && !haveBombs) {
+            System.out.printf("%d bombs left on the field. Sapper position: (%d,%d)",allBombs - bombsHit, row, col);
+        }
+    }
+
+    private static void movePosition(char[][] matrix, int rowMutator, int colMutator) {
+        int nextRow = row + rowMutator;
+        int nextCol = col + colMutator;
+
+
+        if (matrix[nextRow][nextCol] == 'e') {
+            end = true;
+        }
+
+        if (isInBounds(matrix, nextRow,nextCol)) {
+            matrix[row][col] = '+';
+            matrix[nextRow][nextCol] = 's';
+            row = nextRow;
+            col = nextCol;
+        }
+    }
+
+    private static boolean isInBounds(char[][] matrix, int r, int c) {
+        return r >= 0 && r < matrix.length && c >= 0 && c <= matrix[r].length;
+    }
+
+}
---
+++ src/ExamPreparation/FlowerWreaths.java
@@ -0,0 +1,46 @@
+package ExamPreparation;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class FlowerWreaths {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        ArrayDeque<Integer> liliesQueue = new ArrayDeque<>();
+        Arrays.stream(scanner.nextLine().split(", "))
+                .map(Integer::parseInt)
+                .forEach(liliesQueue::push);
+        ArrayDeque<Integer> rosesStack = Arrays.stream(scanner.nextLine().split(", "))
+                        .map(Integer::parseInt)
+                        .collect(Collectors.toCollection(ArrayDeque::new));
+
+        int wreaths = 0;
+        int remainingFlowers = 0;
+
+        while (!rosesStack.isEmpty() && !liliesQueue.isEmpty()) {
+            int currentRoses = liliesQueue.poll();
+            int currentLilies = rosesStack.pop();
+            int sum = currentRoses + currentLilies;
+
+            while (sum > 15) {
+                sum -= 2;
+            }
+            if (sum == 15) {
+                wreaths++;
+            } else {
+                remainingFlowers += sum;
+            }
+        }
+        if (remainingFlowers > 14) {
+            int sumOfAdditionalWreaths = remainingFlowers / 15;
+            wreaths += sumOfAdditionalWreaths;
+        }
+
+        if (wreaths > 4) {
+            System.out.printf("You made it, you are going to the competition with %d wreaths!", wreaths);
+        } else {
+            System.out.printf("You didn't make it, you need %d wreaths more!", 5 - wreaths);
+        }
+    }
+}
