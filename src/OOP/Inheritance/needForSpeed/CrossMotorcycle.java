2a5079ed6ca19e7a9e69e0869b8acab5bbef0cb8
Stanimir <stmbksa@gmail.com>
2022-07-10 23:36:25 +0300

    New problems solved

---
+++ src/OOP/Inheritance/needForSpeed/CrossMotorcycle.java
@@ -0,0 +1,7 @@
+package OOP.Inheritance.needForSpeed;
+
+public class CrossMotorcycle extends Motorcycle{
+    public CrossMotorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
---
+++ src/OOP/Inheritance/needForSpeed/Main.java
@@ -0,0 +1,7 @@
+package OOP.Inheritance.needForSpeed;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
