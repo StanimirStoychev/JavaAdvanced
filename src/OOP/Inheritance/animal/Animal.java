48c2771d3a959b7f8889291ee188e350e0bcc621
Stanimir <stmbksa@gmail.com>
2022-07-31 14:27:24 +0300

    new problems solved in theme : Inheritance

---
+++ src/OOP/Inheritance/animal/Animal.java
@@ -0,0 +1,8 @@
+package OOP.Inheritance.animal;
+
+public class Animal {
+
+    public void eat() {
+        System.out.println("eating...");
+    }
+}
---
+++ src/OOP/Inheritance/animal/Dog.java
@@ -0,0 +1,8 @@
+package OOP.Inheritance.animal;
+
+public class Dog extends Animal {
+
+    public void bark() {
+        System.out.println("barking...");
+    }
+}
