60cd70a89a63befeee5593160e070b369e65dc52
Stanimir <stmbksa@gmail.com>
2023-05-11 14:03:48 +0300

    homework

--- src/Advanced/Generics/Lab/p2_GenericArrayCreator/ArrayCreator.java
+++ src/Advanced/Generics/Lab/p2_GenericArrayCreator/ArrayCreator.java
@@ -4,14 +4,17 @@ import java.lang.reflect.Array;
 
 public class ArrayCreator {
 
+    @SuppressWarnings("unchecked")
     public static <T> T[] create(int length, T item) {
-        T[] array = (T[]) new Object[length];
+        T[] array = (T[]) Array.newInstance(item.getClass(), length);
+
         for (int i = 0; i < array.length; i++) {
             array[i] = item;
         }
         return array;
     }
 
+    @SuppressWarnings("unchecked")
     public static <T> T[] create(Class<T> cl, int length, T item) {
         T[] array = (T[]) Array.newInstance(cl, length);
         for (int i = 0; i < array.length; i++) {
--- src/Advanced/Generics/Lab/p2_GenericArrayCreator/Main.java
+++ src/Advanced/Generics/Lab/p2_GenericArrayCreator/Main.java
@@ -3,12 +3,12 @@ package Advanced.Generics.Lab.p2_GenericArrayCreator;
 public class Main {
     public static void main(String[] args) {
 
-   ////     String[] strings = ArrayCreator.create(10, "none");
+        String[] strings = ArrayCreator.create(10, "none");
         Integer[] integers = ArrayCreator.create(Integer.class, 10, 0);
 
-  //      for (String string : strings) {
-   //       System.out.print(string + " ");
-   //     }
+        for (String string : strings) {
+          System.out.print(string + " ");
+        }
 
         System.out.println();
 
