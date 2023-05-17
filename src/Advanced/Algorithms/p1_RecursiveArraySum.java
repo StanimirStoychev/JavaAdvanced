ce7b48b49c97dcc3a2986787f184604c89f23e50
Stanimir <stmbksa@gmail.com>
2023-05-17 20:03:53 +0300

    new problems solved: Algorithms

---
+++ src/Advanced/Algorithms/p1_RecursiveArraySum.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class p1_RecursiveArraySum {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int[] numbers = Arrays.stream(scanner.nextLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+
+        System.out.println(sum(numbers, 0));
+    }
+
+    private static int sum(int[] numbers, int index) {
+
+        if (index == numbers.length - 1) {
+            return numbers[index];
+        }
+        return numbers[index] + sum(numbers, index + 1);
+    }
+}
